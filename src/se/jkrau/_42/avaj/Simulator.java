bfab987e0ed7c37d556470518e7094ee5d42f8f7
SuperSpyTX <r29jk10@gmail.com>
2018-01-23 19:41:20 -0800

    Push what I have so far

--- src/se/jkrau/_42/avaj/Simulator.java
+++ src/se/jkrau/_42/avaj/Simulator.java
@@ -1,16 +1,32 @@
 package se.jkrau._42.avaj;
 
-import java.io.PrintStream;
+import java.io.*;
 
 public class Simulator {
 
     private WeatherTower weatherTower;
+    private BufferedReader bufferedReader;
     private int iterations;
 
-    public Simulator(WeatherTower tower, int iterations, String outputFile) {
+    public Simulator(WeatherTower tower, int iterations, String inputFile, String outputFile) throws FileNotFoundException {
         this.weatherTower = tower;
         this.iterations = iterations;
+        this.bufferedReader = new BufferedReader(new FileReader(inputFile));
         Logger.getInstance().setOutputFile(outputFile);
+    }
+
+    public boolean parseFile() {
+        try {
+            this.iterations = Integer.parseInt(bufferedReader.readLine());
+
+            String line;
+            while ((line = this.bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public void run() {
